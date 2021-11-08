7b688e6b0ead2f67d7622a8ddc9d77bc232516b7
HEL3X <person_of_paradise@hotmail.com>
2021-11-07 20:31:39 -0800

    Need to check in with you on how to remove brackets when returning an Array. Otherwise it works.

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class GroceryList {
     private String[] arr;
 
@@ -5,21 +7,62 @@ public class GroceryList {
     // Remember to create the array!
     // You don't need to change any of the values in the array
     public GroceryList() {
-        // YOUR CODE HERE
+        arr = new String[10];
     }
-
+    int count = 0;
     // Adds an item to the grocery list
     public void add(String item) {
-        // YOUR CODE HERE
+        if (count < 10){
+            arr[count] = item;
+            count++;
+        }else{
+            System.out.println("Error; Cannot list more than 10 items.");
+        }
     }
 
     // Removes an item from the grocery list
     // Replaces the item with null
     // Remember to shift anything to the left if necessary
     public void remove(String item) {
-        // YOUR CODE HERE
+        for (int i = 0; i < 10; i++) {
+            if (arr[i]==item){
+                arr[i] = null;
+            }
+        }
     }
 
+    //public String[] printOut(){
+      //  int numOfNonNull = 0;
+        //String[] arrOut = new String[numOfNonNull];
+        //for (int i = 0; i < 10; i++) {
+          //  if (arr[i] == "null"){
+            //    numOfNonNull++;
+            //}else{
+              //  arrOut[numOfNonNull] = arr[i];
+            //}
+        //}
+        //return arrOut;
+    //}
+    
+    public static String printOut(String[] arr){
+        int count = 0;
+        int listNum = 0;
+        for (int i = 0; i < 10; i++) {
+            if(arr[i] == null){
+                count++;
+            }
+        }
+        String[] arrOut = new String[10-count];
+        for (int i = 0; i < 10; i++) {
+            if(arr[i] != null){
+                arrOut[listNum] = arr[i];
+                listNum++;
+            }
+        }
+        return Arrays.toString(arrOut);
+    }
+    
+    
     // Returns a String representation the grocery list
     // The String should start with "Grocery List:" and
     // contain each non-null value separated by a comma
@@ -29,7 +72,6 @@ public class GroceryList {
     // You **may** have an extra comma at the end
     @Override
     public String toString() {
-        // YOUR CODE HERE
-        return "";
+        return "Grocery List: " + printOut(arr);
     }
 }
